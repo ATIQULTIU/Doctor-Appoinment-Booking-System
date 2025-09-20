7000a323f76f771985da5b58c4a4884077579e7a
A.i.Atik <134315457+ATIQULTIU@users.noreply.github.com>
2025-09-21 00:41:02 +0600

    Add files via upload

---
+++ scr/assets/assets.js
@@ -0,0 +1,123 @@
+import general from "../assets/general.svg"
+import gastronent from "../assets/Gastroenterologist-CTgzRFeY.svg"
+import gyno from "../assets/Gynecologist-Av1zZu4d.svg"
+import pedi from "../assets/Pediatricians-C6nmx5n8.svg"
+import neu from "../assets/Neurologist-CuaLxNpX.svg"
+import derma from "../assets/derma.svg"
+
+
+import doc1 from "../assets/doc1.png"
+import doc2 from "../assets/doc2.png"
+import doc3 from "../assets/doc3.png"
+import doc4 from "../assets/doc4.png"
+import doc5 from "../assets/doc5.png"
+import doc6 from "../assets/doc6.png"
+import doc7 from "../assets/doc7.png"
+import doc8 from "../assets/doc8.png"
+import doc9 from "../assets/doc9.png"
+import doc10 from "../assets/doc10.png"
+
+export const specialityData= [
+    {
+       speciality: 'General physician',
+       image: general
+    },
+    {
+       speciality: 'Gynecologist',
+       image: gyno
+    },
+    {
+       speciality: 'Dermatologist',
+       image: derma
+    },
+    {
+       speciality: 'Pediatricians',
+       image: pedi
+    },
+    {
+       speciality: 'Neurologist',
+       image: neu
+    },
+    {
+       speciality: 'Gastroenterologist',
+       image:gastronent
+    }
+]
+
+
+export const DoctorInfo = [
+
+   {  _id:'doc1',
+      image:doc1,
+      active:'Available',
+      name:'Dr. Richard James',
+      speciality:'General physician',
+      degree:'MBBS',
+      experience:'4 Years',
+      about:''
+      
+   },
+   {  image:doc2,
+      _id:'doc2',
+      active:'Available',
+      name:'Dr. Emily Larson',
+      speciality:'Gynecologist'
+      
+   },
+   {  image:doc3,
+      _id:'doc3',
+      active:'Available',
+      name:'Dr. Richard James',
+      speciality:'Dermatologist'
+      
+   },
+   {  image:doc4,
+      _id:'doc4',
+      active:'Available',
+      name:'Dr. Sarah Patel',
+      speciality:'General physician'
+      
+   },
+   {  image:doc5,
+      _id:'doc5',
+      active:'Available',
+      name:'Dr. Christopher Lee',
+      speciality:'Pediatricians'
+      
+   },
+   {  image: doc6,
+      _id:'doc6',
+      active:'Available',
+      name:'Dr. Jennifer Garcia',
+      speciality:'Neurologist'
+      
+   },
+   {  image: doc7,
+      _id:'doc7',
+      active:'Available',
+      name:'Dr. Andrew Williams',
+      speciality:'Gastroenterologist'
+      
+   },
+   {  image: doc8,
+      _id:'doc8',
+      active:'Available',
+      name:'Dr. Christopher',
+      speciality:'Gastroenterologist'
+      
+   },
+   {  image: doc9,
+      _id:'doc9',
+      active:'Available',
+      name:'Dr. Timothy White',
+      speciality:'Gynecologist'
+      
+   },
+   {  image: doc10,
+      _id:'doc10',
+      active:'Available',
+      name:'Dr. Ava Mitchell',
+      speciality:'Dermatologist'
+      
+   },
+]
