23c6346f17a9ce58bfecc81ac2631ed15aacc3a7
A.i.Atik <134315457+ATIQULTIU@users.noreply.github.com>
2025-09-21 00:43:16 +0600

    Add files via upload

---
+++ scr/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Speciallity from '../components/Speciallity'
+import TopDoctor from '../components/TopDoctor'
+
+const Home = () => {
+  return (
+    <div>
+     <Header/>
+     <Speciallity/>
+     <TopDoctor/>
+    </div>
+  )
+}
+
+export default Home
